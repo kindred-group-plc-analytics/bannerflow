01b0d62d575075c284566e30101e211699d7a165
Konstantinos Papadopoulos <konpap@lon-fvfxq1prhv2h.unibet.com>
2019-03-18 11:44:06 +0000

    Value Stats fixes

--- tealium/Adobe - doPlugins - BannerFlow pages specific.js
+++ tealium/Adobe - doPlugins - BannerFlow pages specific.js
@@ -17,13 +17,16 @@ bannerFlowProduct = (path.indexOf('bingo') > -1 ? 'bingo' : bannerFlowProduct);
 bannerFlowProduct = (path.indexOf('live-casino') > -1 ? 'live-casino' : bannerFlowProduct);
 bannerFlowProduct = (path.indexOf('lottery') > -1 ? 'lottery' : bannerFlowProduct);
 
+// ValueStats Flag
+var isValueStats = (path.indexOf('valuestats') > -1 ? 'valueStats:yes' : 'valueStats:no');
+
 var market = BF_prop.LanguageCode.toLowerCase() + '_' + BF_prop.RegionCode.toUpperCase();
 
 var project = BF_prop.LandingPageName.toString().replace(/:/ig, '').trim();
 
 var brand = b.adb_site_brand || 'no brand';
 
-b.adb_bannerflow_name = [project, brand, market, bannerFlowMarketingChannel, bannerFlowProduct].join(':');
+b.adb_bannerflow_name = [project, brand, market, bannerFlowMarketingChannel, bannerFlowProduct, isValueStats].join(':');
 b.adb_pageName = 'LP:' + project;
 b.adb_section = 'bannerFlow';
 b.adb_client_id = '';
